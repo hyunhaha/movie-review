43d9e3f012fc6a41e6663975587cd53ea47e1b0f
hyunhaha <gusgk2658@naver.com>
2020-11-30 21:31:12 +0900

    Updates

---
+++ precache-manifest.229b1fcc0c2846a4e1cf88b721e3af5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f772f72b2f35cbb0489b889c59e1bf9",
+    "url": "/movie-review/index.html"
+  },
+  {
+    "revision": "3f6675df0a04ff001377",
+    "url": "/movie-review/static/css/main.63792449.chunk.css"
+  },
+  {
+    "revision": "0c8e0212b893ae420cef",
+    "url": "/movie-review/static/js/2.a3279b69.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/movie-review/static/js/2.a3279b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6675df0a04ff001377",
+    "url": "/movie-review/static/js/main.d25a3399.chunk.js"
+  },
+  {
+    "revision": "b9b18f4edd0028ccf5cb",
+    "url": "/movie-review/static/js/runtime-main.120df9c3.js"
+  }
+]);
